2a81be31e0577ffc34a2c154e517be5d7c67d2c2
ravendraksingh <ravendraksingh@gmail.com>
2019-11-27 17:33:48 +0530

    Separate controller for internal and external APIs

--- order-service/src/main/java/com/rks/orderservice/controller/OrderControllerExternal.java
+++ order-service/src/main/java/com/rks/orderservice/controller/OrderControllerExternal.java
@@ -19,12 +19,9 @@ import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 import static com.rks.orderservice.constants.Constant.*;
 
@@ -37,23 +34,6 @@ public class OrderControllerExternal {
 
     @Autowired
     private OrderService orderService;
-
-    @GetMapping("/orders")
-    public List<Order> getAllOrders() {
-        return orderService.findAllOrders();
-    }
-
-    @GetMapping("/orders/{orderId}")
-    public OrderResponse getOrder(@PathVariable(value="orderId") Long orderId) {
-        return orderService.findOrderById(orderId);
-    }
-
-    @PostMapping("/orders")
-    @ResponseStatus(HttpStatus.CREATED)
-    public OrderResponse createOrder(@RequestBody OrderRequest orderRequest) {
-        return orderService.createNewOrder(orderRequest);
-    }
-
 
     @ApiOperation(
             httpMethod = "GET",
@@ -86,12 +66,85 @@ public class OrderControllerExternal {
     public OrderResponse getOrderWithJwt(@PathVariable("orderId") Long orderId,
                                          @RequestHeader(CLIENT_ID) String clientId,
                                          @RequestHeader(AUTHORIZATION) String jwtToken) {
-        validateToken(clientId, orderId, jwtToken);
+        validateJwtTokenForGetOrder(clientId, orderId, jwtToken);
         return orderService.findOrderById(orderId);
     }
 
 
-    private void validateToken(String clientId, Long orderId, String jwtToken) {
+
+    @ApiOperation(
+            httpMethod = "POST",
+            value = "Create New Order",
+            response = OrderResponse.class,
+            notes = "Please provide access token in the header."
+    )
+    @RequestMapping(
+            value = "/v1/orders",
+            method = RequestMethod.POST,
+            produces = "application/json"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    code = 400,
+                    message = "Bad Request",
+                    response = ErrorResponse.class
+            ),
+            @ApiResponse(
+                    code = 401,
+                    message = "Unauthorized",
+                    response = ErrorResponse.class
+            ),
+            @ApiResponse(
+                    code = 500,
+                    message = "Internal Service Error",
+                    response = ErrorResponse.class
+            )
+    })
+    public OrderResponse createOrderWithJwt(@RequestBody OrderRequest orderRequest,
+                                            @RequestHeader(CLIENT_ID) String clientId,
+                                            @RequestHeader(AUTHORIZATION) String jwtToken) {
+        validateJwtTokenForCreateOrder(clientId, jwtToken);
+        return orderService.createNewOrder(orderRequest);
+    }
+
+    private void validateJwtTokenForGetOrder(String clientId, Long orderId, String jwtToken) {
+        try {
+            String secret = JWT_SECRET_KEY_ORDER_SERVICE;
+            log.info("Verifying jwt token");
+
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("payment-service")
+                    .withClaim("orderId", String.valueOf(orderId))
+                    .withClaim(CLIENT_ID, clientId)
+                    .build();
+
+            jwtVerifier.verify(jwtToken);
+            log.info("Jwt token verification successful");
+        } catch (IllegalArgumentException | UnsupportedEncodingException | JWTVerificationException e) {
+            log.error("Error while verifying JWT token", CommonUtils.exceptionFormatter(e));
+            throw new UnauthorizedAccessException(FAILED, 401, CommonUtils.exceptionFormatter(e));
+        }
+    }
+
+    private void validateJwtTokenForCreateOrder(String clientId, String jwtToken) {
+        try {
+            String secret = JWT_SECRET_KEY_ORDER_SERVICE;
+            log.info("Verifying jwt token");
+
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("payment-service")
+                    .withClaim(CLIENT_ID, clientId)
+                    .build();
+
+            jwtVerifier.verify(jwtToken);
+            log.info("Jwt token verification successful");
+        } catch (IllegalArgumentException | UnsupportedEncodingException | JWTVerificationException e) {
+            log.error("Error while verifying JWT token", CommonUtils.exceptionFormatter(e));
+            throw new UnauthorizedAccessException(FAILED, 401, CommonUtils.exceptionFormatter(e));
+        }
+    }
+
+    /*private void validateJwtTokenForGetOrder(String clientId, Long orderId, String jwtToken) {
         try {
             String secret = JWT_SECRET_KEY_ORDER_SERVICE;
             log.info("Verifying jwt token");
@@ -105,10 +158,6 @@ public class OrderControllerExternal {
             DecodedJWT jwt = jwtVerifier.verify(jwtToken);
             log.info("Jwt token verification successful");
 
-            /*System.out.println(clientId + " : " + orderId);
-            System.out.println(jwt.getClaim(CLIENT_ID).asString());
-            System.out.println(jwt.getClaim("orderId").asString());*/
-
             log.info("Matching Client-id and orderId values provided in header and token");
             if (jwt.getClaim(CLIENT_ID).asString().equalsIgnoreCase(clientId) &&
                     jwt.getClaim("orderId").asString().equals(String.valueOf(orderId))) {
@@ -118,11 +167,7 @@ public class OrderControllerExternal {
         } catch (IllegalArgumentException | UnsupportedEncodingException | JWTVerificationException e) {
             log.error("Error while verifying JWT token", CommonUtils.exceptionFormatter(e));
             throw new UnauthorizedAccessException(FAILED, 401, CommonUtils.exceptionFormatter(e));
-        } /*catch (Exception e) {
-            BaseException ex = new BaseException(FAILED, INTERNAL_SERVER_ERROR_CODE, INTERNAL_SERVER_ERROR_MSG);
-            log.error("Exception occurred while verifying jwt token for Client-id {} and orderId {}. ResponseCode: {}. Message: {}",
-                    clientId, orderId, ex.getCode(), ex.getMessage());
-            throw ex;
-        }*/
-    }
+        }
+    }*/
+
 }
