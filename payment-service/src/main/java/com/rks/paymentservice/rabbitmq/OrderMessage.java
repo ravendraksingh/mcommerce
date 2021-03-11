6c80dda25947a070709d4885af0653e9d9a470d1
ravendraksingh <ravendraksingh@gmail.com>
2021-03-11 22:13:04 +0530

    Feign client enabled

--- payment-service/src/main/java/com/rks/paymentservice/rabbitmq/OrderMessage.java
+++ payment-service/src/main/java/com/rks/paymentservice/rabbitmq/OrderMessage.java
@@ -5,11 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
-public class OrderMessage {
+public class OrderMessage implements Serializable {
     private Long orderId;
     //private Date orderDate;
     private String orderStatus;
--- payment-service/src/main/java/com/rks/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/rks/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,16 +1,19 @@
 package com.rks.paymentservice.service.impl;
 
-import com.rks.paymentservice.client.order.OrderClient;
+import com.rks.paymentservice.clients.orderservice.OrderServiceClient;
+import com.rks.paymentservice.clients.orderservice.OrderServiceClientFeign;
 import com.rks.paymentservice.domain.PaymentMaster;
 import com.rks.paymentservice.dto.order.OrderResponse;
+import com.rks.paymentservice.dto.order.UpdateOrderRequest;
+import com.rks.paymentservice.dto.request.PaymentRequest;
 import com.rks.paymentservice.dto.response.PaymentMasterResponse;
 import com.rks.paymentservice.exceptions.BadRequestException;
 import com.rks.paymentservice.exceptions.BaseException;
 import com.rks.paymentservice.exceptions.NotFoundException;
+import com.rks.paymentservice.mapper.PaymentMapper;
 import com.rks.paymentservice.repository.PaymentMasterRepository;
 import com.rks.paymentservice.service.IPaymentService;
 import com.rks.paymentservice.util.PaymentStatus;
-import com.rks.paymentservice.util.StatusEnum;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 import static com.rks.paymentservice.constants.ErrorCodeConstants.INVALID_PAYMENT_ID;
-import static com.rks.paymentservice.constants.ErrorCodeConstants.ORDER_DETAILS_COULD_NOT_BE_FETCHED;
 
 
 @Service
@@ -33,7 +34,11 @@ public class PaymentServiceImpl implements IPaymentService {
     private PaymentMasterRepository paymentMasterRepository;
 
     @Autowired
-    private OrderClient orderClient;
+    private OrderServiceClient orderServiceClient;
+    @Autowired
+    private OrderServiceClientFeign orderServiceClientFeign;
+    @Autowired
+    private PaymentMapper paymentMapper;
 
     public PaymentServiceImpl(PaymentMasterRepository paymentMasterRepository) {
         this.paymentMasterRepository = paymentMasterRepository;
@@ -55,6 +60,77 @@ public class PaymentServiceImpl implements IPaymentService {
         return createPaymentMasterResponseForPayment(data.get());
     }
 
+
+
+    @Override
+    public PaymentMasterResponse createPayment(PaymentRequest request) {
+        try {
+            log.info("Creating new payment.");
+            PaymentMaster paymentMasterRequest = new PaymentMaster();
+            log.info("Converting and then saving PaymentRequest to PaymentMaster entity");
+            paymentMapper.map(request, paymentMasterRequest);
+            PaymentMaster savedData = paymentMasterRepository.save(paymentMasterRequest);
+            PaymentMasterResponse response = createPaymentMasterResponseForPayment(savedData);
+            updatePaymentStatusInOrder(response.getOrderId());
+            return response;
+        } catch (BaseException e) {
+            BaseException ex = new BaseException(PaymentStatus.PAYMENT_FAILED.getStatus(),1102,"failed payment");
+            log.error("Exception occurred during payment creation for payment id {}. Message: {}", ex.getCode(),
+                    ex.getMessage());
+            throw ex;
+        }
+    }
+
+
+
+    @Override
+    public PaymentMasterResponse createPaymentForNewOrder(Long orderId) {
+        try {
+            log.info("Fetching order details from order-service for orderId {}.", orderId);
+            OrderResponse orderResponse = orderServiceClient.getOrderDetails(orderId);
+
+            log.info("Order details fetched. orderResponse {}.", orderResponse.toString());
+            log.info("Fetching order details from orderResponse");
+
+            PaymentMaster request = createPaymentRequestForOrder(orderResponse);
+            PaymentMaster data = paymentMasterRepository.save(request);
+            updatePaymentStatusInOrder(orderId);
+            return createPaymentMasterResponseForPayment(data);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private void updatePaymentStatusInOrder(Long orderId) {
+        UpdateOrderRequest updateOrderRequest = new UpdateOrderRequest();
+        updateOrderRequest.setPaymentStatus(PaymentStatus.PAYMENT_COMPLETED.getStatus());
+        orderServiceClientFeign.updateOrder(orderId, updateOrderRequest);
+    }
+
+    @Override
+    public OrderResponse getOrderDetailsRemote(Long orderId) {
+        log.info("Going to fetch order details from order service via network call");
+        OrderResponse orderResponse = orderServiceClient.getOrderDetails(orderId);
+        log.info("Response received. orderResponse: {}", orderResponse);
+        return orderResponse;
+    }
+
+    @Override
+    public OrderResponse getOrderDetailsRemoteWithJwt(Long orderId) {
+        log.info("Going to fetch order details from order service via network call");
+        OrderResponse orderResponse = orderServiceClient.getOrderDetailsWithJwt(orderId);
+        log.info("Response received. orderResponse: {}", orderResponse);
+        return orderResponse;
+    }
+
+    @Override
+    public OrderResponse getOrderDetailsRemoteWithJwtToken(Long orderId, String jwtToken) {
+        log.info("Going to fetch order details from order service via network call");
+        OrderResponse orderResponse = orderServiceClient.getOrderDetailsWithJwtToken(orderId, jwtToken);
+        log.info("Response received. orderResponse: {}", orderResponse);
+        return orderResponse;
+    }
+
     private PaymentMasterResponse createPaymentMasterResponseForPayment(PaymentMaster data) {
         PaymentMasterResponse response = new PaymentMasterResponse();
         response.setPaymentId(data.getId());
@@ -64,38 +140,6 @@ public class PaymentServiceImpl implements IPaymentService {
         response.setPaymentStatus(data.getPaymentStatus());
         return response;
     }
-
-    @Override
-    public PaymentMasterResponse createPayment(PaymentMaster request) {
-        try {
-            log.info("Going to create new payment.");
-            PaymentMaster savedData = paymentMasterRepository.save(request);
-            return createPaymentMasterResponseForPayment(savedData);
-        } catch (BaseException e) {
-            BaseException ex = new BaseException(PaymentStatus.PAYMENT_FAILED.getStatus(),1102,"failed payment");
-            log.error("Exception occurred during payment creation for payment id {}. Message: {}", ex.getCode(),
-                    ex.getMessage());
-            throw ex;
-        }
- }
-
-    @Override
-    public PaymentMasterResponse createPaymentForNewOrder(Long orderId) {
-        try {
-            log.info("Fetching order details from order-service for orderId {}.", orderId);
-            OrderResponse orderResponse = orderClient.getOrderDetails(orderId);
-
-            log.info("Order details fetched. orderResponse {}.", orderResponse.toString());
-            log.info("Fetching order details from orderResponse");
-
-            PaymentMaster request = createPaymentRequestForOrder(orderResponse);
-            PaymentMaster data = paymentMasterRepository.save(request);
-            return createPaymentMasterResponseForPayment(data);
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
     private PaymentMaster createPaymentRequestForOrder(OrderResponse orderResponse) {
         log.info("orderIdFromResponse = {} and orderAmount = {}", orderResponse.getOrder_id(), orderResponse.getOrder_amount());
         PaymentMaster requestData = new PaymentMaster();
@@ -104,29 +148,5 @@ public class PaymentServiceImpl implements IPaymentService {
         requestData.setOrderDate(new Date());
         requestData.setPaymentStatus(PaymentStatus.SUCCESS.getStatus());
         return requestData;
-    }
-
-    @Override
-    public OrderResponse getOrderDetailsRemote(Long orderId) {
-        log.info("Going to fetch order details from order service via network call");
-        OrderResponse orderResponse = orderClient.getOrderDetails(orderId);
-        log.info("Response received. orderResponse: {}", orderResponse);
-        return orderResponse;
-    }
-
-    @Override
-    public OrderResponse getOrderDetailsRemoteWithJwt(Long orderId) {
-        log.info("Going to fetch order details from order service via network call");
-        OrderResponse orderResponse = orderClient.getOrderDetailsWithJwt(orderId);
-        log.info("Response received. orderResponse: {}", orderResponse);
-        return orderResponse;
-    }
-
-    @Override
-    public OrderResponse getOrderDetailsRemoteWithJwtToken(Long orderId, String jwtToken) {
-        log.info("Going to fetch order details from order service via network call");
-        OrderResponse orderResponse = orderClient.getOrderDetailsWithJwtToken(orderId, jwtToken);
-        log.info("Response received. orderResponse: {}", orderResponse);
-        return orderResponse;
     }
 }
