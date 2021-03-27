2d56e7225a7482ccce6a848872d2ee72d64d26c4
Ravendra Kumar Singh <ravendraksingh@gmail.com>
2021-03-27 13:39:57 +0530

    Add files via upload

---
+++ mcommerce-gateway/src/main/java/com/rks/gateway/McommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rks.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McommerceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McommerceGatewayApplication.class, args);
+    }
+
+}
